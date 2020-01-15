a9ab9816d88067251e32260998b56489d6e53bb2
Scott Martin <smartin015@gmail.com>
2020-01-15 13:43:06 +0100

    Added placeholder bootstrapping scripts

---
+++ cloud_bootstrap.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "TODO enable the kubernetes API TODO HERE"
+echo "TODO vr/ to host the VR server"
---
+++ local_bootstrap.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+echo "TODO Install the latest version of Ubuntu on your local server"
+echo "TODO  storage/ to start up the local DB container"
+echo "TODO vdi/ to start up the virtual desktop server container"
+echo "TODO rpi/ to start up the PXE / netboot container"
+echo "TODO to start up the app handler container"
+echo "TODO to start up a local DNS server to resolve the above containers"
