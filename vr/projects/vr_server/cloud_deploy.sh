a6c9c44fc3ddb34f6e885ca5dc88da8f5260303d
Scott Martin <smartin015@gmail.com>
2020-01-16 13:29:40 +0100

    Added some side stuff, fixed cloud deploy & vr server

--- vr/projects/vr_server/cloud_deploy.sh
+++ vr/projects/vr_server/cloud_deploy.sh
@@ -3,6 +3,6 @@ gcloud config set project l2-making
 gcloud config set compute/zone us-east4
 gcloud container clusters create l2vr-server --num-nodes=1
 kubectl create deployment l2vr-server --image=gcr.io/l2-making/vr-server
-kubectl expose deployment l2vr-server --type=LoadBalancer --port 44444 --target-port 44444
+kubectl expose deployment l2vr-server --type=LoadBalancer --port 44444 --target-port 44444 --protocol=UDP
 kubectl get pods
 kubectl get service
