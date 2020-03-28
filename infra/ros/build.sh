45215c948358c50e3308dbeebd68c1f72e0907d1
Scott Martin <smartin015@gmail.com>
2020-03-28 10:29:15 +0000

    Set up gazebo with ROS2 eloquent, default world that exports model_state

--- infra/ros/build.sh
+++ infra/ros/build.sh
@@ -3,6 +3,6 @@ ROOT=$(pwd)
 DIRS=$(find . -mindepth 1 -maxdepth 1 -type d -printf '%f\n')
 for d in $DIRS; do
   echo "Building $d as l2$d"
-  cd $ROOT/$d && docker build --tag "l2$d:latest" .
+  docker build --tag "l2$d:latest" ./$d
 done
 cd $ROOT
