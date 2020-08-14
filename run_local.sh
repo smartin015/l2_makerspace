89c32069ada96297be66e303d316af639b603d6b
Scott Martin <smartin015@gmail.com>
2020-08-14 16:35:08 +0200

    Moved volumes to common volumes/ directory so run_local.sh can load correctly from paths

--- run_local.sh
+++ run_local.sh
@@ -7,16 +7,17 @@ set -x
 INFRADIR=$(dirname "$0")/infra
 export COMPOSE_PROJECT_NAME=l2
 docker-compose \
-  -f $INFRADIR/ros/sim/docker-compose.yml \
+  --project-directory $(pwd) \
+  -f $INFRADIR/ros/supervisor/docker-compose.yml \
   -f $INFRADIR/ros/bridge/docker-compose.yml \
   -f $INFRADIR/app/docker-compose.yml \
   -f $INFRADIR/ros/storage/docker-compose.yml \
   -f $INFRADIR/ros/vr/docker-compose.yml \
   -f $INFRADIR/vr/server/docker-compose.yml \
   up --abort-on-container-exit \
-  db storage app vr_syncer app bridge vr_server
-# client app storage db vr_server sdf2 tf_fwd 
-# echo "TODO Starting the local DB container"
+  db storage app vr_syncer app bridge supervisor
+# client app storage db vr_server sdf2 tf_fwd supervisor
+# -f $INFRADIR/ros/sim/docker-compose.yml \
 # echo "TODO vdi/ to start up the virtual desktop server container"
 # echo "TODO rpi/ to start up the PXE / netboot container"
 # echo "TODO to start up the app handler container"
