6d57a7a1b372df9c7df6831b706b424257361ba7
Scott Martin <smartin015@gmail.com>
2020-11-15 18:06:57 -0500

    More rvl cuda testing - added software decode, decoder stats, debug arrays etc

--- infra/ros/sim/rvl/README.md
+++ infra/ros/sim/rvl/README.md
@@ -3,5 +3,13 @@ Realsense sender requires deps:
 ```
 # Follow https://github.com/IntelRealSense/librealsense/issues/6964#issuecomment-707501049 to install python3 librealsense2 on nvidia jetson nano
 
-pip3 install apriltag numba
+# D435 depth resolution 848x480 is recommended (1280x720 for D415)
+# For post processing tips see https://dev.intelrealsense.com/docs/depth-post-processing
+# Should add edge-preserving filter (using disparity domain filtering) and skip holes in time
+
+# numba may require installing conda (see also conda4aarch64 conda installer)
+# https://numba.pydata.org/numba-doc/latest/user/installing.html#installing-on-linux-armv8-aarch64-platforms
+# Conda must be python3.6 for builtin cv2 to work (`conda install python=3.6`)
+
+pip3 install apriltag
 ```
--- infra/ros/sim/rvl/rvl_cuda.py
+++ infra/ros/sim/rvl/rvl_cuda.py
@@ -48,52 +48,86 @@ print("Kernel bounds: {}, thread shape: {}".format(KB, CUDA_THREAD_DIM))
 # =========== CALCULATIONS ===========
 # CUDA kernel. For each thread
 @cuda.jit
-def rvl_kernel(im, out):
+def rvl_kernel(im, out, dbg):
     x, y = cuda.grid(2)
     outx = CUDA_THREAD_DIM[0]*y + x # Linear output id
     x = KB[0]*x
     y = KB[1]*y
-    im[x][y] += 1
+    # im[x][y] += 1
+
+    # Variable length encoding tracking
+    word = 0
+    nibblesWritten = 0
     
-    # TODO preamble
-    outIdx = 0
-    out[outx][outIdx] = 5
-    outIdx += 1
-    
+    # Write XY top left corner in pixel space
+    out[outx][0] = (y << 16) | x
+    outIdx = 1
+
+    dbgIdx = 1    
     i = 0
     while i < PX_KB:
+        dbg[outx][0] += 1 # Num iterations
+        # Count zeros and nonzeros
         zeros = 0
-        zstop = False
-        zadd = 1
-        while i < PX_KB and zadd:
-            ix = x + (i % KB[0])
-            iy = y + (i // KB[0])
-            zadd = (im[ix][iy] == 0) # Or below delta
-            zeros += zadd
-            i += 1
-        # TODO variable length encoding
-        out[outx][outIdx] = zeros
-        outIdx += 1
-        
         nonzeros = 0
-        nzadd = 1
-        while i+nonzeros < PX_KB and nzadd:
+        z = 1
+        while i < PX_KB and (zeros == 0 or nonzeros == 0 or not z):
             ix = x + (i % KB[0])
             iy = y + (i // KB[0])
-            nzadd = (im[ix][iy] != 0) # And above delta
-            nonzeros += nzadd
+            z = (im[ix][iy] == 0) # Or below delta
+            zeros += z
+            nonzeros += (not z)
             i += 1
-        out[outx][outIdx] = nonzeros
-        outIdx += 1
-    
-        #j = 0
-        #while j < nonzeros:
-        #    # TODO delta, VLE
-        #    jx = x + (j % KB[0])
-        #    jy = y + (i // KB[0])
-        #    out[outx][outIdx] = im[jx][jy]
-        #    outIdx += 1
+        
+        # Write out segment (zeros, nonzeros, values[])
+        j = i - nonzeros
+        while j < i and outIdx < PX_KB:
+            if zeros != -1:
+                value = zeros
+                zeros = -1
+                dbg[outx][dbgIdx] = value
+                dbgIdx += 1
+            elif nonzeros != -1:
+                value = nonzeros
+                nonzeros = -1
+                dbg[outx][dbgIdx] = value
+                dbgIdx += 1
+            else:
+                jx = x + (j % KB[0])
+                jy = y + (j // KB[0])
 
+                # TODO add filtering, delta, time stuff
+                value = im[jx][jy]
+                j += 1
+
+            # Embed negative values in positive space 
+            # sequence is 0, 1, -1, 2, -2...
+            value = (value << 1) ^ (value >> 31)
+
+            # Write data (variable length encoding)
+            while True:
+                # Pop lower 3 bits from value
+                nibble = value & 0x7
+                value = (value >> 3)
+
+                # Indicate more data if some value remains
+                if value != 0:
+                    nibble |= 0x8
+                
+                # Append nibble to word
+                word = (word << 4) | nibble 
+                nibblesWritten += 1
+
+                # Flush word when complete (Tegra is 64bit architecture)
+                if nibblesWritten == 16: 
+                    out[outx][outIdx] = word
+                    outIdx += 1
+                    nibblesWritten = 0
+                    word = 0
+
+                # We're done when there's no more data
+                if value == 0:
+                    break
      #TODO flush
     
 
@@ -102,7 +136,9 @@ from datetime import datetime
 
 timings = []
 # One row per thread, with length allowing maximum compression size
-result = np.zeros((NTHREADS, PX_KB+1), dtype=np.uint16)
+# TODO size PX_KB properly
+result = np.zeros((NTHREADS, PX_KB+1), dtype=np.uint64)
+dbg = np.zeros((NTHREADS, 256), dtype=np.int16) # TODO remove for better performance
 
 pipeline = rs.pipeline()
 config = rs.config()
@@ -110,8 +146,14 @@ rs.config.enable_device_from_file(config, "./a.bag")
 #config.enable_stream(rs.stream.depth, dim[0], dim[1], rs.format.z16, 30)
 pipeline.start(config)
 
+ctx = cuda.current_context()
+ctx.reset()
+print(ctx.get_memory_info())
+
 print("Running on img shape {}, output shape {} (Ctl+C to stop)...".format(dim, result.shape))
-while True:
+sample = None
+nsamp = 0
+while nsamp < 3:
     try:
         has, frame = pipeline.try_wait_for_frames()
         if not has:
@@ -119,14 +161,95 @@ while True:
         print(frame)
         start = datetime.now()
         data = np.asanyarray(frame.get_depth_frame().get_data())
-        rvl_kernel[block_per_grid, CUDA_THREAD_DIM](data, result)
+        rvl_kernel[block_per_grid, CUDA_THREAD_DIM](data, result, dbg)
         timings.append(datetime.now() - start)
+        sample = data[0:KB[0]][0:KB[1]]
+        nsamp += 1
     except KeyboardInterrupt:
         break
 
-print("Result ({} initial, {} post-avg, {} samples):\n{}".format(timings[0], np.mean(timings[1:]), len(timings[1:]), result[0:8][0:20]))
 
+def decodeSector(sector):
+    x = int(sector[0]) & 0xffff
+    y = (int(sector[0]) >> 16) & 0xffff
+    print("decoding sector {}, {}".format(x,y))
+
+    i = 1
+    word = 0
+    nibblesWritten = 0
+
+    dbg = [0]
+
+    zeros = None
+    nonzeros = None
+    written = 0
+    result = np.zeros(KB, dtype=np.uint16)
+    while i < len(sector):
+        # Decode variable
+        nibble = 0
+        value = 0
+        bits = 61 # 64 - 3 bits
+        while True:
+            if not nibblesWritten:
+                word = int(sector[i])
+                i += 1
+                nibblesWritten = 16
+            nibble = word & 0xf000000000000000
+            value = value | (nibble << 1) >> bits
+            word = (word << 4)
+            nibblesWritten -= 1
+            bits -= 3
+            if not (nibble & 0x8000000000000000):
+                break
+            
+        # Value is now assigned; reverse positive flipping from compression
+        value = (value >> 1) ^ -(value & 1)
+        if zeros is None:
+            zeros = value
+            written += zeros
+        elif nonzeros is None:
+            nonzeros = value
+        elif zeros == 0 and nonzeros == 0:
+            return (result, dbg)
+        elif written < nonzeros:
+            result[written % KB[0]][written // KB[0]] = value
+            written += 1
+        else:
+            dbg[0] += 1 # Num sections written
+            dbg.append(zeros)
+            dbg.append(nonzeros)
+            zeros = None
+            nonzeros = None
+            written = 0
+    return result
+
+
+
+
+cuda.close()
+# np.set_printoptions(threshold=np.inf)
+print("{} result ({} initial, {} post-avg, {} samples):\n"
+      "top left quadrant\n{}\npre-encoded\n{}".format(
+          result.shape, timings[0], np.mean(timings[1:]), len(timings[1:]), 
+          sample, dbg[0]))
+
+# Count zeroes and nonzeros from debug info
+pre_zeros = 0
+pre_nonzeros = 0
+for i in range(1, len(dbg[0])):
+    if i % 2:
+        pre_nonzeros += dbg[0][i]
+    else:
+        pre_zeros += dbg[0][i]
+sample_nonzeros = np.count_nonzero(sample)
+print("pre-encoded sums: %d zeros, %d zeros\nactual image: %d zeros, %d nonzeros" % (pre_zeros, pre_nonzeros, len(sample)-sample_nonzeros, sample_nonzeros))
+
+decoded, decodeDbg = decodeSector(result[0])
+print("decode dbg\n{}\ndecoded\n{}".format(decodeDbg, decoded))
+
+cv2.imshow("sample encoded", sample / np.max(sample))
 cv2.imshow("img", result / np.max(result))
+cv2.imshow("sample decoded", decoded / np.max(decoded))
 cv2.waitKey(0)
 
 # Compute thread image bounds Ioffs, Iwidth, Iheight = (threadID * stride, M/(stride1/N), N/(stride2/M))
