c22b15076ee30cd48cec7d30e7f25dcb9a1d4805
Scott Martin <smartin015@gmail.com>
2020-12-06 21:26:44 -0500

    actually add rvl cuda receiver/sender

---
+++ infra/ros/depth/rvl_cuda_receive.py
@@ -0,0 +1,56 @@
+from numba import cuda
+import rvl_cuda
+from math import sqrt, ceil
+import numpy as np
+import cv2
+import pyrealsense2 as rs
+import socket
+from datetime import datetime
+
+DEPROJECT = False
+SECTOR_BATCH = 3584
+SECTOR_LEN = 64
+dim = (480, 848)
+UDP = ("0.0.0.0", 19823)
+BUFSZ = SECTOR_LEN*8 # Recieve buffer size, accounting for byte->uint64
+NUM_SECTOR = 3584
+KB = (8, 16)
+rvl_cuda.configure(KB, NUM_SECTOR)
+
+def serve_forever(sock):
+    encoded = {}
+    decoded = {}
+    i = 0
+    print("UDP server listening")
+    while(True):
+        (data, src) = sock.recvfrom(BUFSZ)
+        if len(data) != 512:
+            continue
+
+        src=src[0]
+        if encoded.get(src) is None:
+            print("New client", src)
+            encoded[src] = np.zeros((SECTOR_BATCH, SECTOR_LEN), dtype=np.uint64)
+            decoded[src] = np.zeros(dim, dtype=np.uint16)
+
+        encoded[src][i,:] = np.frombuffer(data, dtype=np.uint64)
+        i += 1
+        if i >= SECTOR_BATCH:
+            rvl_cuda.decode[4, int(SECTOR_BATCH/4)](encoded[src], decoded[src], DEPROJECT)
+            i = 0
+            cv2.imshow(src, decoded[src] / max(1, np.max(decoded[src])))
+            cv2.waitKey(1)
+
+if __name__ == "__main__":
+    #import argparse
+    #parser = argparse.ArgumentParser()
+    #parser.add_argument("res")
+    #args = parser.parse_args()
+    sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    sock.bind(UDP)
+    try: 
+        serve_forever(sock)
+    finally: 
+        cuda.close()
+
+
---
+++ infra/ros/depth/rvl_cuda_send.py
@@ -0,0 +1,52 @@
+from numba import cuda
+import rvl_cuda
+from math import sqrt, ceil
+import numpy as np
+import cv2
+import pyrealsense2 as rs
+import socket
+from datetime import datetime
+
+DEPROJECT = False
+SECTOR_BATCH = 256
+dim = (480, 848)
+UDP = ("0.0.0.0", 19823)
+BLOCKS_PER_GRID = (8, 7)
+THREADS_PER_BLOCK = (8, 8)
+NUM_SECTOR = 3584
+KB = (8, 16)
+rvl_cuda.configure(KB, NUM_SECTOR)
+print(rvl_cuda.SECTOR_LEN)
+
+def send_loop(sock):
+    original = np.zeros(dim, dtype=np.uint16)
+    encoded = np.zeros((NUM_SECTOR, rvl_cuda.SECTOR_LEN), dtype=np.uint64)
+    pipeline = rs.pipeline()
+    config = rs.config()
+    rs.config.enable_device_from_file(config, './a.bag')
+    pipeline.start(config)
+    while(True):
+        try:
+            has, frame = pipeline.try_wait_for_frames()
+            if not has:
+                break
+            print(frame)
+            data = np.asanyarray(frame.get_depth_frame().get_data())
+            rvl_cuda.encode[BLOCKS_PER_GRID, THREADS_PER_BLOCK](data, encoded)
+            for i in range(encoded.shape[0]):
+                sock.sendto(encoded[i,:].tobytes(), UDP)
+        except KeyboardInterrupt:
+            return
+
+if __name__ == "__main__":
+    #import argparse
+    #parser = argparse.ArgumentParser()
+    #parser.add_argument("res")
+    #args = parser.parse_args()
+    sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    try: 
+        send_loop(sock)
+    finally: 
+        cuda.close()
+
+
