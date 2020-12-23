196f79bcf7f3f5fcb75d264748544e91cccdff51
Scott Martin <smartin015@gmail.com>
2020-12-23 16:54:00 -0500

    First semi-working version of moveit_servo using new configs

---
+++ ar3/sim/config/servo.launch.py
@@ -0,0 +1,61 @@
+import os
+import yaml
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import ComposableNodeContainer
+from launch_ros.descriptions import ComposableNode
+
+YAML_PATH = 'servo.yaml'
+URDF_PATH = 'ar2.urdf'
+SRDF_PATH = 'ar2.srdf'
+
+def generate_launch_description():
+    # TODO build configs into package
+    # package_path = get_package_share_directory('l2_ar3')
+    package_path = '/volume/config/'
+
+    # Get parameters for the Servo node
+    with open(os.path.join(package_path, YAML_PATH), 'r') as f:
+        servo_yaml = yaml.safe_load(f)
+    servo_params = {'moveit_servo' : servo_yaml}
+    print(servo_params)
+
+    # Get URDF and SRDF
+    with open(os.path.join(package_path, URDF_PATH), 'r') as f:
+        robot_description_config = f.read()
+    robot_description = {'robot_description' : robot_description_config}
+
+    with open(os.path.join(package_path, SRDF_PATH), 'r') as f:
+        robot_description_semantic_config = f.read()
+    robot_description_semantic = {'robot_description_semantic' : robot_description_semantic_config}
+
+    # Launch as much as possible in components
+    container = ComposableNodeContainer(
+            name='ar2_servo',
+            namespace='/',
+            package='rclcpp_components',
+            executable='component_container',
+            composable_node_descriptions=[
+                ComposableNode(
+                    package='robot_state_publisher',
+                    plugin='robot_state_publisher::RobotStatePublisher',
+                    name='robot_state_publisher',
+                    parameters=[robot_description]),
+                ComposableNode(
+                    package='tf2_ros',
+                    plugin='tf2_ros::StaticTransformBroadcasterNode',
+                    name='static_tf2_broadcaster',
+                    parameters=[ {'/child_frame_id' : 'base_link', '/frame_id' : 'world'} ]),
+                ComposableNode(
+                    package='moveit_servo',
+                    plugin='moveit_servo::ServoServer',
+                    name='servo_server',
+                    parameters=[servo_params, robot_description, robot_description_semantic],
+                    extra_arguments=[{'use_intra_process_comms' : True}])
+            ],
+            output='screen',
+    )
+    
+    return LaunchDescription([container])
+
--- ar3/sim/node.py
+++ ar3/sim/node.py
@@ -2,11 +2,15 @@
 import os
 import rclpy
 from rclpy.node import Node
+from rclpy.qos import qos_profile_sensor_data
+from tf2_ros import TransformBroadcaster, StaticTransformBroadcaster
 from controller import Robot, Motor, PositionSensor
 from std_msgs.msg import Float64
+from trajectory_msgs.msg import JointTrajectory
 from rosgraph_msgs.msg import Clock
 from builtin_interfaces.msg import Time
 from sensor_msgs.msg import JointState
+from geometry_msgs.msg import TransformStamped
 import threading
 
 targets = [
@@ -31,7 +35,7 @@ class AR3(Node):
         # Setup robot
         self.robot = Robot()
         self.timestep = int(self.robot.getBasicTimeStep())
-        self.joint_names = ["joint_%d" % i for i in range(1, self.NUM_JOINTS)]
+        self.joint_names = ["joint_%d" % (i+1) for i in range(self.NUM_JOINTS)]
         self.motors = [self.robot.getMotor(n) for n in self.joint_names]
         self.sensors = [m.getPositionSensor() for m in self.motors]
         for s in self.sensors:
@@ -39,19 +43,41 @@ class AR3(Node):
 
         # Setup topics & timers
         self.ti = 0
+        self.stbc = StaticTransformBroadcaster(self)
+        self.tbc = TransformBroadcaster
         self.pub = self.create_publisher(Float64, 'sense', 10)
-        self.joint_state_pub = self.create_publisher(JointState, 'joint_state', 10)
+        self.joint_state_pub = self.create_publisher(JointState, 'joint_states', 10)
         # self.timer = self.create_timer(self.PUBLISH_PD, self.timer_callback)
         self.last_joint_state_ts = self.get_clock().now()
         self.last_joint_state = [0 for i in self.joint_names]
         self.joint_state_timer = self.create_timer(self.JOINT_STATE_PD, self.joint_state_callback)
         # self.dance_timer = self.create_timer(self.DANCE_PD, self.dance_callback)
+        self.command_sub = self.create_subscription(JointTrajectory, "joint_trajectory_command", self.handle_joint_trajectory, qos_profile=qos_profile_sensor_data)
         self.get_logger().info("Init")
 
     def spin_sim(self):
         while self.robot.step(self.timestep) != -1:
             now = self.robot.getTime()
             self.clockpub.publish(Clock(clock=Time(sec=int(now), nanosec=int((now % 1) * 1000000000))))
+
+    def handle_joint_trajectory(self, jt):
+        print(jt) # TODO handle setting joint trajectory
+
+
+    def mktf(self, parent, child, xyz, quat):
+        tfs = TransformStamped()
+        tfs.header.stamp = self.get_clock().now().to_msg()
+        tfs.header.frame_id = parent
+        tfs.child_frame_id = child
+        tfs.transform.translation.x = float(xyz[0])
+        tfs.transform.translation.y = float(xyz[1])
+        tfs.transform.translation.z = float(xyz[2])
+        tfs.transform.rotation.x = float(quat[0])
+        tfs.transform.rotation.y = float(quat[1])
+        tfs.transform.rotation.z = float(quat[2])
+        tfs.transform.rotation.w = float(quat[3])
+        return tfs
+
 
     def joint_state_callback(self):
         pos = [s.getValue() for s in self.sensors]
@@ -70,6 +96,9 @@ class AR3(Node):
         ))
         self.last_joint_state_ts = now
         self.last_joint_state = pos
+        
+        # self.stbc.sendTransform(self.mktf("world", "base_link", (0,0,0), (0,0,0,1)))
+
 
     def dance_callback(self):
         self.ti = (self.ti + 1) % len(targets)
