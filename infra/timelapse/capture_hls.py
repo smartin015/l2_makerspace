5b9cb7d6d9a8ef94233cde6047631b98f0d9edc7
Scott Martin <smartin015@gmail.com>
2021-12-01 19:41:20 -0500

    Added mqtt to capture_hls

--- infra/timelapse/capture_hls.py
+++ infra/timelapse/capture_hls.py
@@ -2,8 +2,9 @@ import threading
 import time
 from urllib.request import urlopen
 from urllib.error import URLError, HTTPError
+import paho.mqtt.client as mqtt
 
-def parseurl(url: str, outfile: str, already_parsed: list):
+def parseurl(url: str, outfile: str, already_parsed: list, client):
   try:
     response = [r.strip() for r in urlopen(url).read().decode("utf-8").split("\n")]
   except URLError: # No response
@@ -19,23 +20,32 @@ def parseurl(url: str, outfile: str, already_parsed: list):
         f.write(urlopen(line).read())
       except HTTPError:
         continue
+    client.publish("/av/status/capture", line)
     print(line, "DONE")
   return parsed
 
-def record(url, outfile):
+def record(url, outfile, client):
     print("Listening on", url," --> ", outfile)
     last_parsed = set()
     t = threading.current_thread()
     while t.alive:
-      last_parsed = parseurl(url, outfile, last_parsed)
+      last_parsed = parseurl(url, outfile, last_parsed, client)
       time.sleep(5.0)
 
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected to MQTT with result code "+str(rc))
 
 if __name__ == "__main__":
+  print("Starting telemetry...")
+  client = mqtt.Client()
+  client.on_connect = on_connect
+  client.connect("mqtt", 1883, 60)
+
   threads = [
-    threading.Thread(target=record, args=['http://l2:8080/playlist.m3u8', 'l2_out.ts']),
-    threading.Thread(target=record, args=['http://jetson1:8080/playlist.m3u8', 'jetson1_out.ts']),
-    threading.Thread(target=record, args=['http://jetson2:8080/playlist.m3u8', 'jetson2_out.ts']),
+    threading.Thread(target=record, args=['http://l2:8080/playlist.m3u8', 'l2_out.ts', client]),
+    threading.Thread(target=record, args=['http://jetson1:8080/playlist.m3u8', 'jetson1_out.ts', client]),
+    threading.Thread(target=record, args=['http://jetson2:8080/playlist.m3u8', 'jetson2_out.ts', client]),
   ]
   print("Starting threads...")
   for t in threads:
@@ -43,8 +53,9 @@ if __name__ == "__main__":
     t.daemon = True
     t.start()
 
+  print("Running(Ctrl+C to stop)")
   try:
-    input()
+    client.loop_forever()
   except KeyboardInterrupt:
     pass
   print("Stopping threads...")
